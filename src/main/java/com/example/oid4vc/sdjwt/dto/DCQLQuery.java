0b8af5ac27888e4d6b5bc40c94f097fd6ca20d86
shlee1223 <shlee1223@raoncorp.com>
2025-09-15 13:00:55 +0900

    -스웨거 제거

--- src/main/java/com/example/oid4vc/sdjwt/dto/DCQLQuery.java
+++ src/main/java/com/example/oid4vc/sdjwt/dto/DCQLQuery.java
@@ -2,7 +2,6 @@ package com.example.oid4vc.sdjwt.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
